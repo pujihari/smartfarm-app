11057895149a7446b420615096976076616e07a6
[dyad] <shakadigital.id@gmail.com>
2025-09-17 22:32:18 +0700

    [dyad] Memperbaiki error API key Supabase yang invalid - wrote 2 file(s)

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  supabaseUrl: 'https://zrnsngrdhvgcgwjdhgrf.supabase.co',
-  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpybnNuZ3JkaHZnY2d3amRoZ3JmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTU1Nzg3ODgsImV4cCI6MjA3MTE1NDc4OH0.kYLwoE_XQwyIKAmt_Q1o_HqSAXKCshITrK-6jDYRVVA'
+  supabaseUrl: 'https://[PROJECT-ID-YANG-BARU].supabase.co',
+  supabaseKey: '[ANON-KEY-YANG-BARU]'
 };
